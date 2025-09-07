699c4f1ea4d9746fa64c9e29f315f7d69dd32eff
mahesh <Maheshfullstack1@gmail.com>
2025-09-07 08:50:10 +0530

    javascript

---
+++ 7.functionswithparams.js
@@ -0,0 +1,26 @@
+//functiom with params and arguments.
+function getaddition(param1 , param2){
+    let firstum = param1;
+    let second  = param2;
+
+    let output = firstum + secondnum;
+        return output;
+}
+//sutraction
+function getsubtraction(param1 , param2){
+    let firstum = param1;
+    let second  = param2;
+
+    let output = firstum -secondnum;
+        return output;
+
+}
+
+
+
+var caldata = getaddition(10,20);  // calling the function
+console.log("first dara",caldata);
+
+
+var caldata1 = getaddition(30,40);
+console.log("second data",caldata);
