7c7f8051646cea684f0c15eb9e61621366619be2
mahesh <Maheshfullstack1@gmail.com>
2025-09-07 07:40:50 +0530

    fuction demo2

--- 4functions .js
+++ 4functions .js
@@ -5,9 +5,9 @@ function firstfuction()
     console.timeLog("hello world 1");
     console.timeLog("hello world 2");
 }
-
-firstfuction();
 // the way you are created the function declartion
+firstfuction();
+
 function secondfunctio()
 {
     console.timeLog("hello world 1");
@@ -17,16 +17,18 @@ function secondfunctio()
 secondfuction();
 function thirdfunctio()
 {
-    let studentslist = [{
-        "studentname": "Mahesh"
-        "location"   : "kadapa"
+    let studentslist = [
+        {
+        "studentname": "Mahesh",
+        "location"   : "kadapa",
         "Aadharcard" : "1234-xxxx-7890" 
-       }
+       },
     {
-        "studentname": "Manju"
-        "location"   : "kadapa"
-        "Aadharcard" : "1234-xxxx-7890" 
-    }]
+        "studentname" : "Manju",
+        "location"    : "vellatur",
+        "Aadharcard" : "7890-xxxx-3214" 
+    }];
+    // let studetslist  ["mahesh" , "manju"];
     console.log(studentslist);
 }
 
